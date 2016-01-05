2ce9c216663d7d65ae1b1b9a2f0ba3d4efe8625b
JanewzhWang <Jane.Wang@software.dell.com>
2016-01-05 15:12:09 +0800

    add network firewall support
    add network firewall support

--- src/main/java/org/dasein/cloud/qingcloud/network/QingCloudNetworkFirewall.java
+++ src/main/java/org/dasein/cloud/qingcloud/network/QingCloudNetworkFirewall.java
@@ -1,53 +1,35 @@
-/*
- *  *
- *  Copyright (C) 2009-2015 Dell, Inc.
- *  See annotations for authorship information
- *
- *  ====================================================================
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *  ====================================================================
- *
- */
 package org.dasein.cloud.qingcloud.network;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import org.dasein.cloud.CloudException;
 import org.dasein.cloud.InternalException;
 import org.dasein.cloud.ResourceStatus;
 import org.dasein.cloud.VisibleScope;
 import org.dasein.cloud.dc.DataCenter;
-import org.dasein.cloud.network.AbstractFirewallSupport;
+import org.dasein.cloud.network.AbstractNetworkFirewallSupport;
 import org.dasein.cloud.network.Direction;
 import org.dasein.cloud.network.Firewall;
-import org.dasein.cloud.network.FirewallCapabilities;
 import org.dasein.cloud.network.FirewallCreateOptions;
 import org.dasein.cloud.network.FirewallRule;
 import org.dasein.cloud.network.FirewallRuleCreateOptions;
-import org.dasein.cloud.network.FirewallSupport;
+import org.dasein.cloud.network.NetworkFirewallCapabilities;
+import org.dasein.cloud.network.NetworkFirewallSupport;
 import org.dasein.cloud.network.Permission;
 import org.dasein.cloud.network.Protocol;
 import org.dasein.cloud.network.RuleTarget;
 import org.dasein.cloud.network.Subnet;
 import org.dasein.cloud.qingcloud.QingCloud;
+import org.dasein.cloud.qingcloud.model.ResponseModel;
 import org.dasein.cloud.qingcloud.model.SimpleJobResponseModel;
 import org.dasein.cloud.qingcloud.network.model.AddSecurityGroupRulesResponseModel;
 import org.dasein.cloud.qingcloud.network.model.CreateSecurityGroupResponseModel;
 import org.dasein.cloud.qingcloud.network.model.DeleteSecurityGroupRulesResponseModel;
 import org.dasein.cloud.qingcloud.network.model.DeleteSecurityGroupsResponseModel;
 import org.dasein.cloud.qingcloud.network.model.DescribeSecurityGroupRulesResponseModel;
-import org.dasein.cloud.qingcloud.network.model.DescribeSecurityGroupRulesResponseModel.DescribeSecurityGroupRulesResponseItemModel;
 import org.dasein.cloud.qingcloud.network.model.DescribeSecurityGroupsResponseModel;
+import org.dasein.cloud.qingcloud.network.model.DescribeSecurityGroupRulesResponseModel.DescribeSecurityGroupRulesResponseItemModel;
 import org.dasein.cloud.qingcloud.network.model.DescribeSecurityGroupsResponseModel.DescribeSecurityGroupsResponseItemModel;
 import org.dasein.cloud.qingcloud.util.requester.QingCloudDriverToCoreMapper;
 import org.dasein.cloud.qingcloud.util.requester.QingCloudRequestBuilder;
@@ -55,18 +37,12 @@ import org.dasein.cloud.qingcloud.util.requester.QingCloudRequester;
 import org.dasein.cloud.util.APITrace;
 import org.dasein.cloud.util.requester.fluent.Requester;
 
-/**
- * Created by Jane Wang on 12/16/2015.
- *
- * @author Jane Wang
- * @since 2016.02.1
- */
-public class QingCloudFirewall extends
-		AbstractFirewallSupport<QingCloud> implements FirewallSupport {
+public class QingCloudNetworkFirewall extends
+		AbstractNetworkFirewallSupport<QingCloud> implements NetworkFirewallSupport {
 
 	private static final Integer DefaultResponseDataLimit = 999;
 	
-	protected QingCloudFirewall(QingCloud provider) {
+	protected QingCloudNetworkFirewall(QingCloud provider) {
 		super(provider);
 	}
 
@@ -76,8 +52,8 @@ public class QingCloudFirewall extends
 			Protocol protocol, RuleTarget destinationEndpoint, int beginPort,
 			int endPort, int precedence) throws CloudException,
 			InternalException {
-	
-		APITrace.begin(getProvider(), "QingCloudFirewall.authorize");
+		
+		APITrace.begin(getProvider(), "QingCloudNetworkFirewall.authorize");
 		try {
 			
 			if (firewallId == null) {
@@ -107,7 +83,7 @@ public class QingCloudFirewall extends
 				throw new InternalException("Authorize firewall rule failed!");
 			}
 		
-			applySecurityGroup(response.getSecurityGroupRules().get(0), null);
+			applySecurityGroup(response.getSecurityGroupRules().get(0));
 			return response.getSecurityGroupRules().get(0);
 		} finally {
 			APITrace.end();
@@ -115,10 +91,10 @@ public class QingCloudFirewall extends
 	}
 
 	@Override
-	public String create(FirewallCreateOptions options)
+	public String createFirewall(FirewallCreateOptions options)
 			throws InternalException, CloudException {
-
-		APITrace.begin(getProvider(), "QingCloudFirewall.create");
+		
+		APITrace.begin(getProvider(), "QingCloudNetworkFirewall.create");
 		try {
 		
 			QingCloudRequestBuilder requestBuilder = QingCloudRequestBuilder.get(getProvider()).action("CreateSecurityGroup");
@@ -141,66 +117,37 @@ public class QingCloudFirewall extends
 				}
 			}
 			
-//			if (options.getProviderVlanId() != null) {
-//				
-//				requestBuilder = QingCloudRequestBuilder.get(getProvider()).action("ModifyRouterAttributes");
-//				requestBuilder.parameter("router", options.getProviderVlanId());
-//				requestBuilder.parameter("security_group", response.getSecurityGroupId());
-//				requestBuilder.parameter("zone", getProviderDataCenterId());
-//				Requester<ResponseModel> modifyRouterAttributesRequester = new QingCloudRequester<ResponseModel, ResponseModel>(
-//		        		getProvider(), 
-//		        		requestBuilder.build(), 
-//		        		ResponseModel.class);
-//				modifyRouterAttributesRequester.execute();
-//		        
-//				applySecurityGroup(response.getSecurityGroupId(), null);
-//			}
+			//associate network firewall to router
+			if (options.getProviderVlanId() != null) {
+				requestBuilder = QingCloudRequestBuilder.get(getProvider()).action("ModifyRouterAttributes");
+				requestBuilder.parameter("router", options.getProviderVlanId());
+				requestBuilder.parameter("security_group", response.getSecurityGroupId());
+				requestBuilder.parameter("zone", getProviderDataCenterId());
+				Requester<ResponseModel> modifyRouterAttributesRequester = new QingCloudRequester<ResponseModel, ResponseModel>(
+		        		getProvider(), 
+		        		requestBuilder.build(), 
+		        		ResponseModel.class);
+				modifyRouterAttributesRequester.execute();
+				applySecurityGroup(response.getSecurityGroupId());
+			}
+			
 			return response.getSecurityGroupId();
 			
 		} finally {
 			APITrace.end();
 		}
 	}
-	
-	@Override
-	public void delete(String firewallId) throws InternalException,
-			CloudException {
-		
-		APITrace.begin(getProvider(), "QingCloudFirewall.delete");
-		try {
-		
-			if (firewallId == null) {
-				throw new InternalException("Invalid firewall id!");
-			}
-			
-			QingCloudRequestBuilder requestBuilder = QingCloudRequestBuilder.get(getProvider()).action("DeleteSecurityGroups");
-			requestBuilder.parameter("security_groups.1", firewallId);
-			requestBuilder.parameter("zone", getProviderDataCenterId());
-			Requester<DeleteSecurityGroupsResponseModel> requester = new QingCloudRequester<DeleteSecurityGroupsResponseModel, DeleteSecurityGroupsResponseModel>(
-	        		getProvider(), 
-	        		requestBuilder.build(), 
-	        		DeleteSecurityGroupsResponseModel.class);
-			DeleteSecurityGroupsResponseModel response = requester.execute();
-			if (response == null || response.getSecurityGroups() == null || 
-					response.getSecurityGroups().size() == 0 || !response.getSecurityGroups().get(0).equals(firewallId)) {
-				throw new InternalException("Delete firewall " + firewallId + " failed!");
-			}
-			
-		} finally {
-			APITrace.end();
-		}
-	}
 
 	@Override
-	public FirewallCapabilities getCapabilities() throws CloudException,
+	public NetworkFirewallCapabilities getCapabilities() throws CloudException,
 			InternalException {
-		return new QingCloudFirewallCapabilities(getProvider());
+		return new QingCloudNetworkFirewallCapabilities(getProvider());
 	}
 
 	@Override
 	public Firewall getFirewall(String firewallId) throws InternalException,
 			CloudException {
-		APITrace.begin(getProvider(), "QingCloudFirewall.getFirewall");
+		APITrace.begin(getProvider(), "QingCloudNetworkFirewall.getFirewall");
 		try {
 			QingCloudRequestBuilder requestBuilder = QingCloudRequestBuilder.get(getProvider()).action("DescribeSecurityGroups");
 			requestBuilder.parameter("verbose", 1);
@@ -222,33 +169,29 @@ public class QingCloudFirewall extends
 	}
 
 	@Override
-	public Iterable<FirewallRule> getRules(String firewallId)
+	public boolean isSubscribed() throws CloudException, InternalException {
+		return true;
+	}
+
+	@Override
+	public Iterable<ResourceStatus> listFirewallStatus()
 			throws InternalException, CloudException {
-		APITrace.begin(getProvider(), "QingCloudFirewall.getRules");
+		APITrace.begin(getProvider(), "QingCloudNetworkFirewall.listFirewallStatus");
 		try {
-			QingCloudRequestBuilder requestBuilder = QingCloudRequestBuilder.get(getProvider()).action("DescribeSecurityGroupRules");
-			requestBuilder.parameter("security_group", firewallId);
-			requestBuilder.parameter("limit", DefaultResponseDataLimit);
-			requestBuilder.parameter("zone", getProviderDataCenterId());
-			Requester<List<FirewallRule>> subnetRequester = new QingCloudRequester<DescribeSecurityGroupRulesResponseModel, List<FirewallRule>>(
-	                getProvider(), 
-	                requestBuilder.build(), 
-	                new FirewallRulesMapper(), 
-	                DescribeSecurityGroupRulesResponseModel.class);
-			return subnetRequester.execute();
+			List<ResourceStatus> statuses = new ArrayList<ResourceStatus>();
+			for ( Firewall firewall : listFirewalls()) {
+				statuses.add(new ResourceStatus(firewall.getProviderFirewallId(), true));
+			}
+			return statuses;
 		} finally {
 			APITrace.end();
 		}
 	}
 
 	@Override
-	public boolean isSubscribed() throws CloudException, InternalException {
-		return true;
-	}
- 
-	@Override
-	public Iterable<Firewall> list() throws InternalException, CloudException {
-		APITrace.begin(getProvider(), "QingCloudFirewall.list");
+	public Iterable<Firewall> listFirewalls() throws InternalException,
+			CloudException {
+		APITrace.begin(getProvider(), "QingCloudNetworkFirewall.listFirewalls");
 		try {
 			QingCloudRequestBuilder requestBuilder = QingCloudRequestBuilder.get(getProvider()).action("DescribeSecurityGroups");
 			requestBuilder.parameter("verbose", 1);
@@ -266,15 +209,45 @@ public class QingCloudFirewall extends
 	}
 
 	@Override
-	public Iterable<ResourceStatus> listFirewallStatus()
+	public Iterable<FirewallRule> listRules(String firewallId)
 			throws InternalException, CloudException {
-		APITrace.begin(getProvider(), "QingCloudFirewall.listFirewallStatus");
+		APITrace.begin(getProvider(), "QingCloudNetworkFirewall.listRules");
 		try {
-			List<ResourceStatus> statuses = new ArrayList<ResourceStatus>();
-			for ( Firewall firewall : list()) {
-				statuses.add(new ResourceStatus(firewall.getProviderFirewallId(), true));
+			QingCloudRequestBuilder requestBuilder = QingCloudRequestBuilder.get(getProvider()).action("DescribeSecurityGroupRules");
+			requestBuilder.parameter("security_group", firewallId);
+			requestBuilder.parameter("limit", DefaultResponseDataLimit);
+			requestBuilder.parameter("zone", getProviderDataCenterId());
+			Requester<List<FirewallRule>> subnetRequester = new QingCloudRequester<DescribeSecurityGroupRulesResponseModel, List<FirewallRule>>(
+	                getProvider(), 
+	                requestBuilder.build(), 
+	                new FirewallRulesMapper(), 
+	                DescribeSecurityGroupRulesResponseModel.class);
+			return subnetRequester.execute();
+		} finally {
+			APITrace.end();
+		}
+	}
+
+	@Override
+	public void removeFirewall(String... firewallIds) throws InternalException,
+			CloudException {
+		APITrace.begin(getProvider(), "QingCloudNetworkFirewall.removeFirewall");
+		try {
+		
+			if (firewallIds == null && firewallIds.length == 0) {
+				return;
 			}
-			return statuses;
+			
+			QingCloudRequestBuilder requestBuilder = QingCloudRequestBuilder.get(getProvider()).action("DeleteSecurityGroups");
+			for (int i = 0; i < firewallIds.length; i++) {
+				requestBuilder.parameter("security_groups." + (i + 1), firewallIds[i]);
+			}
+			requestBuilder.parameter("zone", getProviderDataCenterId());
+			Requester<DeleteSecurityGroupsResponseModel> requester = new QingCloudRequester<DeleteSecurityGroupsResponseModel, DeleteSecurityGroupsResponseModel>(
+	        		getProvider(), 
+	        		requestBuilder.build(), 
+	        		DeleteSecurityGroupsResponseModel.class);
+			requester.execute();
 		} finally {
 			APITrace.end();
 		}
@@ -283,7 +256,7 @@ public class QingCloudFirewall extends
 	@Override
 	public void revoke(String providerFirewallRuleId) throws InternalException,
 			CloudException {
-		APITrace.begin(getProvider(), "QingCloudFirewall.revoke");
+		APITrace.begin(getProvider(), "QingCloudNetworkFirewall.revoke");
 		try {
 			QingCloudRequestBuilder requestBuilder = QingCloudRequestBuilder.get(getProvider()).action("DeleteSecurityGroupRules");
 			requestBuilder.parameter("security_group_rules.1", providerFirewallRuleId);
@@ -302,6 +275,15 @@ public class QingCloudFirewall extends
 		}
 	}
 
+	@Override
+	public String getProviderTermForNetworkFirewall(Locale locale) {
+		try {
+			return getCapabilities().getProviderTermForNetworkFirewall(locale);
+		} catch (Exception e) {
+			throw new RuntimeException("Find term for network firewall through capabilities failed!");
+		}
+	}
+	
 	private String getProviderDataCenterId() throws InternalException, CloudException {
 		String regionId = getContext().getRegionId();
         if (regionId == null) {
@@ -311,16 +293,11 @@ public class QingCloudFirewall extends
         return dataCenters.iterator().next().getProviderDataCenterId();//each account has one DC in each region
 	}
 	
-	private void applySecurityGroup(String securityGroupId, List<String> instanceIds) 
+	private void applySecurityGroup(String securityGroupId) 
 			throws InternalException, CloudException {
 		QingCloudRequestBuilder requestBuilder = QingCloudRequestBuilder.get(getProvider()).action("ApplySecurityGroup");
 		requestBuilder.parameter("security_group", securityGroupId);
 		requestBuilder.parameter("zone", getProviderDataCenterId());
-		if (instanceIds != null && instanceIds.size() > 0) {
-			for (int i = 0; i < instanceIds.size(); i++) {
-				requestBuilder.parameter("instances." + (i + 1), instanceIds.get(i));
-			}
-		}
 		Requester<SimpleJobResponseModel> requester = new QingCloudRequester<SimpleJobResponseModel, SimpleJobResponseModel>(
         		getProvider(), 
         		requestBuilder.build(), 
@@ -352,7 +329,7 @@ public class QingCloudFirewall extends
 						}
 						firewall.setRegionId(getContext().getRegionId());
 						firewall.setVisibleScope(VisibleScope.ACCOUNT_DATACENTER);
-						firewall.setRules(mapFromFirewallRules(getRules(firewall.getProviderFirewallId())));
+						firewall.setRules(mapFromFirewallRules(listRules(firewall.getProviderFirewallId())));
 						firewalls.add(firewall);
 					}
 				}
@@ -451,4 +428,5 @@ public class QingCloudFirewall extends
 			return null;
 		}
 	}
+	
 }
